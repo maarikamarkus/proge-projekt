584070d6d981d062445214206f686d3a0f2cf933
maarikamarkus <maarika.markus@gmail.com>
2019-11-07 11:27:34 +0200

    täiendasin veidi lisainfo ja tegevustega

--- projekt.py
+++ projekt.py
@@ -7,6 +7,7 @@
 # sellele moodulile tuleb ise URL-e sööta, siis teeb neist soup-objecti
 
 # https://www.youtube.com/watch?v=ng2o98k983k -- hea video beautifulsoupi kohta
+# https://www.youtube.com/watch?v=87Gx3U0BDlo -- veel head infot
 
 # cinamon
 # class = "schedule_film_name" - kinokavas oleva filmi class
@@ -21,8 +22,22 @@ import requests
 # request.get() abil saame veebilehe kätte
 source = requests.get("https://cinamonkino.com/tasku/").text
 
+# status_code, kui == 200, siis saame ligi sellele lehele
+#print(source.status_code)
+
 # BeautifulSoup annab meile selle lehe html koodi
 soup = BeautifulSoup(source, 'lxml')
 
-# soup.prettify() teeb html koodile õiged taanded
-print(soup.prettify())
+# .prettify() teeb html koodile õiged taanded
+#print(soup.prettify())
+
+# find_all, 'a' leiab kõik lingid sellelt lehelt
+links = soup.find_all('a')
+#print(links)
+
+# otsime lingi, mille sisu on Kinokava ja siis võtame selle href-i väärtuse ka
+for link in links:
+    if "Kinokava" in link.text:
+        print(link)
+        print(link.attrs['href'])
+        
