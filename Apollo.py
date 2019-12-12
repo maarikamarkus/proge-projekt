63a58be7042dbe8b366b55eca9c0b34e051c0acb
Kati Ilus <katiilus@Katis-MBP.lan>
2019-12-12 20:37:27 +0200

    Sain Apollo faili tööle

--- Apollo.py
+++ Apollo.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
 def apollo_aeg_film(apollo_src):
     apollo_kinokava = BeautifulSoup(apollo_src, 'lxml')
@@ -8,28 +9,21 @@ def apollo_aeg_film(apollo_src):
     print("Kino Apollo tänane kinokava on: ")
     apollo_aeg_film = {}
     for apollo_rida in apollo_kavarida:
-        #print(apollo_rida)
         apollo_film = apollo_rida.find('h2', class_="list-item-desc-title")
-        apollo_filminimi = sapollo_film.find("a").text.strip()
-        #print(apollo_film)
-        print(apollo_filminimi)
         apollo_kellaaeg = apollo_rida.find("div", class_="btn-group")
-        apollo_kell = (apollo_kellaaeg.find("span")).text.strip()
-        #print (apollo_kellaaeg)
-        print(apollo_kell)
-        #apollo_aeg_film[apollo_kellaaeg.text.strip()] = apollo_film.text.strip()
-    #for apollo_el in apollo_aeg_film:
-        #print(apollo_el + " : " + apollo_aeg_film[apollo_el])
-    print("\n")
+        try:
+            apollo_filminimi = apollo_film.find("a", href = True).text.strip()
+            apollo_filminimi =re.sub(" +", " ", apollo_filminimi)
+            apollo_kell = (apollo_kellaaeg.find("span")).text.strip()
+            if apollo_kell == "Tähestikuline":
+                a = 0
+            else:
+                apollo_aeg_film[apollo_kell]=apollo_filminimi
+                print(apollo_kell, ":" , apollo_filminimi)
+        except:
+            break 
+
 
 
 apollo_src = requests.get("https://www.apollokino.ee/?TheatreArea=1014&SetHome=1").text
-print(apollo_aeg_film(apollo_src))
-
-#apollo_kavarida = apollo_kinokava.find_all('h2', class_="list-item-desc-title")
-
-#print("Kino Apollo tänane kinokava on: ")
-#for apollo_rida in apollo_kavarida:
-#    apollo_film = apollo_rida.find('a', href = True).text.strip()
-#    print(apollo_film)
-#print("\n")
+apollo_aeg_film = apollo_aeg_film(apollo_src)
