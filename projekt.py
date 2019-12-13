d114b999a5dd76efba4633f3f77027d5d1ef7ae5
maarikamarkus <maarika.markus@gmail.com>
2019-12-13 10:40:45 +0200

    lisasin kahe kino nupud GUI-sse

--- projekt.py
+++ projekt.py
@@ -65,17 +65,55 @@ def ekraan():
 
 
 #APOLLO
-#apollo_src = requests.get("https://www.apollokino.ee").text
-#
-#apollo_kinokava = BeautifulSoup(apollo_src, 'lxml')
-#
-#apollo_kavarida = apollo_kinokava.find_all('h2', class_="list-item-desc-title")
-#
-#print("Kino Apollo tänane kinokava on: ")
-#for apollo_rida in apollo_kavarida:
-#    apollo_film = apollo_rida.find('a', href = True).text.strip()
-#    print(apollo_film)
+def apollo_aeg_film(apollo_src):
+    apollo_kinokava = BeautifulSoup(apollo_src, 'lxml')
+    apollo_kavarida = apollo_kinokava.find_all('div', class_="panel-body")
 
+    print("Kino Apollo Eedeni tänane kinokava on: ")
+    for apollo_rida in apollo_kavarida:
+        apollo_film = apollo_rida.find('h2', class_="list-item-desc-title")
+        apollo_kellaaeg = apollo_rida.find("div", class_="btn-group")
+        try:
+            apollo_filminimi = apollo_film.find("a", href = True).text.strip()
+            apollo_filminimi =re.sub(" +", " ", apollo_filminimi)
+            apollo_kell = (apollo_kellaaeg.find("span")).text.strip()
+            if apollo_kell == "Tähestikuline":
+                a = 0
+            else:
+                print(apollo_kell, ":" , apollo_filminimi)
+        except:
+            break
+    print("\n")
+apollo_src = requests.get("https://www.apollokino.ee/?TheatreArea=1014&SetHome=1").text
+
+def apollo():
+    apollo = apollo_aeg_film(apollo_src)
+    
+# LÕUNAKESKUS
+def lounakeskus_aeg_film(lounakeskus_src):
+    lounakeskus_kinokava = BeautifulSoup(lounakeskus_src, 'lxml')
+    lounakeskus_kavarida = lounakeskus_kinokava.find_all('div', class_="panel-body")
+
+    print("Kino Apollo Lõunakeskuse tänane kinokava on: ")
+    for lounakeskus_rida in lounakeskus_kavarida:
+        lounakeskus_film = lounakeskus_rida.find('h2', class_="list-item-desc-title")
+        lounakeskus_kellaaeg = lounakeskus_rida.find("div", class_="btn-group")
+        try:
+            lounakeskus_filminimi = lounakeskus_film.find("a", href = True).text.strip()
+            lounakeskus_filminimi =re.sub(" +", " ", lounakeskus_filminimi)
+            lounakeskus_kell = (lounakeskus_kellaaeg.find("span")).text.strip()
+            if lounakeskus_kell == "Tähestikuline":
+                a = 0
+            else:
+                print(lounakeskus_kell, ":" , lounakeskus_filminimi)
+        except:
+            break
+    print("\n")
+
+lounakeskus_src = requests.get("https://www.apollokino.ee/?TheatreArea=1011&SetHome=1").text
+
+def lounakeskus():
+    lounakeskus = lounakeskus_aeg_film(lounakeskus_src)
 
 # Filmi otsing
 
@@ -107,9 +145,14 @@ cinamon = Button(menüü, text="Cinamoni kinokava", command=cinamon)
 cinamonkava = Label(menüü, text=cinamon)
 cinamon.pack(side=LEFT)
 
-
 ekraan = Button(menüü, text="Ekraani kinokava", command=ekraan)
 ekraan.pack(side=LEFT)
+
+apollo = Button(menüü, text="Apollo kinokava", command=apollo)
+apollo.pack(side=LEFT)
+
+lounakeskus = Button(menüü, text="Lõunakeskuse kinokava", command=lounakeskus)
+lounakeskus.pack(side=LEFT)
 
 menüü.pack(side=TOP, fill=X)
 
